e9fc9157f51ce6102ea906b809ac21a75f6f5c31
www <www@DESKTOP-G73F02P>
2023-05-03 11:09:07 +0530

    Add project files.

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [MaxLength(length:100)]
+        public string? Description { get; set; }
+
+        [Required,
+        MaxLength(length:13)]
+        public string? ISBN { get; set; }
+
+        [Required]
+        public string? Author { get; set; }
+
+        [Required]
+        public string? Language { get; set; }
+
+        [Display(Name ="Image URL")]
+        public string? ImageUrl { get; set; }
+
+        [Required,
+        DataType(DataType.Date),
+        Display(Name ="Date Published")]
+        public DateTime DatePublished { get; set; }
+
+        [Required,
+        DataType(DataType.Currency)    ]
+        public int Price { get; set; }
+
+    }
+}
---
+++ BookStore/Models/SData.cs
@@ -0,0 +1,67 @@
+﻿//using BookStore.Data;
+//using Microsoft.EntityFrameworkCore;
+
+
+//namespace BookStore.Models
+//{
+//    public class SData
+//    {
+//        public static void Initialize(IServiceProvider serviceProvider)
+//        {
+//            using (var context = new BookStoreContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreContext>>()))
+//            {
+//                if (context.Books.Any())
+//                {
+//                    return;
+//                }
+//                    context.Books.AddRange(
+//                        new Book
+//                        {
+//                            Title = "Harrypotter",
+//                            Language = "en",
+//                            ISBN = "15456445354",
+//                            DatePublished = DateTime.Parse("2023-9-14"),
+//                            Price = 100,
+//                            Author = "Asric",
+//                            ImageUrl = ""
+//                        },
+
+//                         new Book
+//                         {
+//                             Title = "Harrypotter2",
+//                             Language = "en",
+//                             ISBN = "15456445455",
+//                             DatePublished = DateTime.Parse("2023-10-14"),
+//                             Price = 100,
+//                             Author = "Asric",
+//                             ImageUrl = ""
+//                         },
+
+//                          new Book
+//                          {
+//                              Title = "Harrypotter4",
+//                              Language = "en",
+//                              ISBN = "15456445456",
+//                              DatePublished = DateTime.Parse("2023-11-14"),
+//                              Price = 100,
+//                              Author = "Asric",
+//                              ImageUrl = ""
+//                          },
+
+//                           new Book
+//                           {
+//                               Title = "Harrypotter3",
+//                               Language = "en",
+//                               ISBN = "15456445457",
+//                               DatePublished = DateTime.Parse("2023-12-14"),
+//                               Price = 100,
+//                               Author = "Asric",
+//                               ImageUrl = ""
+//                           }
+//                        );
+//                    context.SaveChanges();
+//                }
+//            }
+//        }
+//    }
+
